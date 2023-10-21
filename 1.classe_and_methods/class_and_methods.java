7cf9922ccedc731258ac08816bc1910809582ade
Agatha Barros <agathabarros@gmail.com>
2023-10-21 08:44:29 +0100

    first commit

---
+++ 1.classe_and_methods/class_and_methods.java
@@ -0,0 +1,10 @@
+// Definição de uma classe
+class Car {
+    String make;
+    String model;
+}
+
+// Criação de objetos a partir da classe
+Car car1 = new Car();
+car1.make = "Toyota";
+car1.model = "Camry";
